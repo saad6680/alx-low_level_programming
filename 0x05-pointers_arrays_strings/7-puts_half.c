6a72a66264a0b9bb9ac69cb14b7268df643e6332
saad6680 <saadezz645@gmail.com>
2023-03-28 13:39:20 -0700

    Winning

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of strings
+ * @str:function parameter
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int n = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
+	int i;
+	for (i = n; i < length; i++)
+	{
+	printf("%c", str[i]);
+	}
+	printf("\n");
+}
